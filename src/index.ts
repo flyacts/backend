6c85e5ac81bfba494f6bc4035afaa7fb806573e0
Tim Schumacher <tim.schumacher@flyacts.com>
2019-01-24 11:01:24 +0100

    feat: integrate cls-hooked for RequestContext

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,6 @@
 export { VersionInformation } from './interfaces/version-information.interface';
 export { Logger } from './providers/logger.provider';
 export { Backend } from './backend';
+export { RequestContext } from './helpers/request-context';
+export { BaseError } from './errors/base.error';
+export { NoActiveSessionError } from './errors/no-active-session.error';
