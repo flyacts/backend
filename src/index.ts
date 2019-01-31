f0d3e00c919e22b90d8b97e1a24f92e7e0467e0e
Tim Schumacher <tim.schumacher@flyacts.com>
2019-01-31 15:21:45 +0100

    fix: move the request-context to extra package
    This avoids circular dependencies

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,5 @@
 export { VersionInformation } from './interfaces/version-information.interface';
 export { Logger } from './providers/logger.provider';
 export { Backend } from './backend';
-export { RequestContext } from './helpers/request-context';
 export { BaseError } from './errors/base.error';
 export { NoActiveSessionError } from './errors/no-active-session.error';
