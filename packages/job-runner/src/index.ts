4bd737d4ee0de4c8ab9ca5dec533788b388eb605
Tim Schumacher <tim.schumacher@flyacts.com>
2019-08-05 10:32:29 +0200

    fix: export job.entity

--- packages/job-runner/src/index.ts
+++ packages/job-runner/src/index.ts
@@ -4,6 +4,7 @@
 
 export { JobQueueCallback } from './entities/job-queue.entity';
 export { JobQueueEntity } from './entities/job-queue.entity';
+export { JobEntity } from './entities/job.entity';
 export { JobStatus } from './enums/job-status.enum';
 export { JobManagement1554452540011 } from './migrations/1554452540011-job-management';
 export { JobManager } from './providers/job-manager.provider';
