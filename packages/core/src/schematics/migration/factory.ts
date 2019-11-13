3686d1429f225b3a43054415a4bcbeddda5a172a
Tim Schumacher <tim.schumacher@flyacts.com>
2019-11-13 09:40:11 +0100

    feat(schematics): add a schematic for migrations

---
+++ packages/core/src/schematics/migration/factory.ts
@@ -0,0 +1,40 @@
+/*!
+ * @copyright FLYACTS GmbH 2019
+ */
+
+import { strings } from '@angular-devkit/core';
+import {
+    apply,
+    applyTemplates,
+    mergeWith,
+    Rule,
+    SchematicContext,
+    Tree,
+    url,
+} from '@angular-devkit/schematics';
+import moment = require('moment');
+const pluralize = require('pluralize');
+
+export interface OptionSchema {
+    name: string;
+}
+
+/**
+ * Scaffold the migration
+ */
+// tslint:disable-next-line:no-default-export
+export default function(options: OptionSchema): Rule {
+    return (_tree: Tree, _context: SchematicContext) => {
+        const now = moment();
+        const source = apply(url('./files'), [
+            applyTemplates({
+                ...options,
+                ...strings,
+                pluralize,
+                timestamp: +(now.toDate()),
+                now,
+            }),
+        ]);
+        return mergeWith(source);
+    };
+}
