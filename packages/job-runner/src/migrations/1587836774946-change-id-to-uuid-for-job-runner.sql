6766770454a8c43b0abaccccba1b3c4ba4ff0323
Daniel Kuehn <daniel.kuehn@flyacts.com>
2020-05-25 17:53:54 +0200

    fix: job queue id renaming

--- packages/job-runner/src/migrations/1587836774946-change-id-to-uuid-for-job-runner.sql
+++ packages/job-runner/src/migrations/1587836774946-change-id-to-uuid-for-job-runner.sql
@@ -7,7 +7,7 @@ ALTER TABLE public.jobs ADD COLUMN uuid_id uuid DEFAULT uuid_generate_v4();
 ALTER TABLE public.jobs ADD COLUMN uuid_job_queue_id uuid;
 UPDATE public.jobs SET uuid_job_queue_id = (SELECT uuid_id FROM public.job_queues WHERE id = job_queue_id);
 ALTER TABLE public.jobs DROP COLUMN job_queue_id;
-ALTER TABLE public.jobs RENAME uuid_job_queue_id TO job_queues_id;
+ALTER TABLE public.jobs RENAME uuid_job_queue_id TO job_queue_id;
 
 ALTER TABLE public.jobs DROP COLUMN id;
 ALTER TABLE public.jobs RENAME COLUMN uuid_id TO id;
@@ -17,7 +17,7 @@ ALTER TABLE public.job_queues DROP COLUMN id;
 ALTER TABLE public.job_queues RENAME COLUMN uuid_id TO id;
 ALTER TABLE public.job_queues ADD CONSTRAINT pk___job_queues___id PRIMARY KEY (id);
 
-ALTER TABLE public.jobs DROP CONSTRAINT IF EXISTS fk___jobs___job_queues_id___job_queue;
-ALTER TABLE public.jobs ADD CONSTRAINT fk___jobs___job_queues_id___job_queue FOREIGN KEY (job_queue_id)
+ALTER TABLE public.jobs DROP CONSTRAINT IF EXISTS fk___jobs___job_queue_id___job_queue;
+ALTER TABLE public.jobs ADD CONSTRAINT fk___jobs___job_queue_id___job_queue FOREIGN KEY (job_queue_id)
     REFERENCES public.job_queues (id) MATCH FULL
     ON DELETE NO ACTION ON UPDATE NO ACTION;
