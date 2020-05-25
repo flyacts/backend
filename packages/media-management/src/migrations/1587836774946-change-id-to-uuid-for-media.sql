9275d796c8bd0a55e16b653725918fd43a2446d9
Daniel Kuehn <daniel.kuehn@flyacts.com>
2020-05-25 17:41:24 +0200

    fix: remove job contraints from media sql

--- packages/media-management/src/migrations/1587836774946-change-id-to-uuid-for-media.sql
+++ packages/media-management/src/migrations/1587836774946-change-id-to-uuid-for-media.sql
@@ -17,10 +17,5 @@ ALTER TABLE public.media DROP COLUMN id;
 ALTER TABLE public.media RENAME COLUMN uuid_id TO id;
 ALTER TABLE public.media ADD CONSTRAINT pk___media___id PRIMARY KEY (id);
 
-ALTER TABLE public.jobs DROP CONSTRAINT IF EXISTS fk___jobs___job_queues_id___job_queue;
-ALTER TABLE public.jobs ADD CONSTRAINT fk___jobs___job_queues_id___job_queue FOREIGN KEY (job_queue_id)
-    REFERENCES public.job_queues (id) MATCH FULL
-    ON DELETE NO ACTION ON UPDATE NO ACTION;
-
 ALTER TABLE public.media RENAME COLUMN model_id TO legacy_model_id;
 ALTER TABLE public.media ADD COLUMN model_id uuid;
